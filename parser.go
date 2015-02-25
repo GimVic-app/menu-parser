4aaf409ba5df76ea81f48a21b8de43f57d94ce03
Žiga Patačko Koderman <ziga.patacko@gmail.com>
2015-02-25 18:23:05 +0100

    init

---
+++ parser.go
@@ -0,0 +1,183 @@
+package main
+
+import (
+	"fmt"
+	"os"
+    "encoding/csv"
+    "encoding/json"
+    "io/ioutil"
+    "strings"
+    "time"
+    "database/sql"
+    _ "github.com/go-sql-driver/mysql"
+)
+
+type Kosilo struct{
+	Navadno []string
+	Vegetarijansko []string
+}
+
+type Malica struct{
+	Navadna []string
+	VegSPerutnino []string
+	Vegetarijanska []string
+}
+
+func main() {
+	
+	//check for file argument
+    if len(os.Args) < 2 {
+    	fmt.Println("Specifiy the file!")
+    } else {
+    	fileArg := os.Args[1]
+
+    	if isJedilnikValid(fileArg) {
+    		fmt.Println("Parsing file " + fileArg)
+    	
+ 	   		csvfile, err := os.Open(fileArg)
+    		defer csvfile.Close()
+  	  		check(err)
+    	
+    		reader := csv.NewReader(csvfile)
+    		reader.Comma = ';'
+    		reader.FieldsPerRecord = -1
+    		rawCSVdata, err := reader.ReadAll()
+    		check(err)
+
+
+    		secNumbers, malica := getSectionNumbers(rawCSVdata)
+
+    		if(malica){
+    			processMalica(rawCSVdata, secNumbers)
+    		}else {
+    			processKosilo(rawCSVdata, secNumbers)
+    		}
+
+    	}else {
+    		panic("Invalid jedilnik!")
+    	}
+    }   
+}
+
+//provides table sections and type (malica = true, kosilo = false)
+func getSectionNumbers(csv [][]string) ([]int, bool){
+	var result []int
+	var malica bool
+
+	for i, line := range csv {
+		if(strings.Contains(line[1], "navadna") || strings.Contains(line[1], "kosilo")){
+			result = append(result, i)
+			if strings.Contains(line[1], "navadna"){
+				malica = true
+			}
+		}
+	}
+
+	return result, malica
+}
+
+func processMalica(table [][]string, selNumbers []int) {
+	for i, num := range selNumbers{
+		if i+1 == len(selNumbers){
+			processMalicaSel(table[num:len(table)])
+		} else {
+			processMalicaSel(table[num:selNumbers[i+1]])
+		}
+	}
+
+	fmt.Println("Pareser done processing malica.")
+}
+
+func processMalicaSel(sel [][]string) {
+	date := findDate(sel)
+
+	temp := new(Malica)
+	for i := 1; i < len(sel); i++{
+		if(sel[i][1] != ""){
+			temp.Navadna = append(temp.Navadna, sel[i][1])
+		}
+		if(sel[i][3] != ""){
+			temp.VegSPerutnino = append(temp.VegSPerutnino, sel[i][3])
+		}
+		if(sel[i][5] != ""){
+			temp.Vegetarijanska = append(temp.Vegetarijanska, sel[i][5])
+		}
+	}
+
+	jsonData, _ := json.Marshal(temp)
+
+	con, err := sql.Open("mysql", "app:urnikZAvse@/app")
+	check(err)
+	defer con.Close()
+
+	_, err = con.Exec("insert into malica (date, json) values (?, ?)", date, jsonData)
+	check(err)
+
+}
+
+func processKosiloSel(sel [][]string) {
+	date := findDate(sel)
+
+	temp := new(Kosilo)
+	for i := 1; i < len(sel); i++{
+		if(sel[i][1] != ""){
+			temp.Navadno = append(temp.Navadno, sel[i][1])
+		}
+		if(sel[i][3] != ""){
+			temp.Vegetarijansko = append(temp.Vegetarijansko, sel[i][3])
+		}
+	}
+
+	jsonData, _ := json.Marshal(temp)
+
+	con, err := sql.Open("mysql", "app:urnikZAvse@/app")
+	check(err)
+	defer con.Close()
+
+	_, err = con.Exec("insert into kosilo (date, json) values (?, ?)", date, jsonData)
+	check(err)
+
+}
+
+func processKosilo(table [][]string, selNumbers []int) {
+	for i, num := range selNumbers{
+		if i+1 == len(selNumbers){
+			processKosiloSel(table[num:len(table)])
+		} else {
+			processKosiloSel(table[num:selNumbers[i+1]])
+		}
+	}
+
+	fmt.Println("Pareser done processing kosilo.")
+}
+
+func findDate(sel [][]string) time.Time {
+	var index int = 0
+	for i, line := range sel {
+		cell := strings.ToLower(line[0])
+		if strings.Contains(cell, "pon") || strings.Contains(cell, "tor") || strings.Contains(cell, "sre") || strings.Contains(cell, "rtek") || strings.Contains(cell, "pet") {
+			index = i + 1;
+		}
+	}
+
+	date, err := time.Parse("2.1.2006", sel[index][0])
+	check(err)
+	return date	
+}
+
+func isJedilnikValid(fileName string) bool {
+	csv, err := ioutil.ReadFile(fileName)
+    check(err)
+
+    if strings.Count(string(csv), ";") == 240  && (strings.Contains(strings.ToLower(string(csv)), "navadna") || strings.Contains(strings.ToLower(string(csv)), "kosilo")){
+    	return true
+    }
+    return false
+}
+
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
